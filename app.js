3f98183efda35d8f9b8b938503826b2242fb3d90
Afsin <56025376+Afsinur@users.noreply.github.com>
2023-01-19 20:46:18 +0600

    init4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || `1000`;
+const port = process.env.PORT || `1000`;
 const listen = () => {
   console.log(`listening on port: ${port}`);
 };
